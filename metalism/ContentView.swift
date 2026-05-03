3427a47607ac00da496ee3d8d2fbc5d20a79d1c7
Usman Nazir <usmannzr@live.com>
2026-05-04 00:30:59 +0100

    Initial Commit

---
+++ metalism/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metalism
+//
+//  Created by Usman Nazir on 04/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ metalism/metalismApp.swift
@@ -0,0 +1,17 @@
+//
+//  metalismApp.swift
+//  metalism
+//
+//  Created by Usman Nazir on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct metalismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
